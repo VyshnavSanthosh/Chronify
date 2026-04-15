b17a35b726793483903632af32e00b294aef07bb
VyshnavSanthosh <vyshnavsanthosh5@gmail.com>
2026-04-15 20:38:20 +0530

    fix profile validation issues

--- src/utils/validators/joi_profile.js
+++ src/utils/validators/joi_profile.js
@@ -16,13 +16,13 @@ export default Joi.object({
 
     lastName: Joi.string()
         .trim()
-        .min(2)
+        .min(1)
         .max(50)
         .pattern(/^[A-Za-z]+$/)
         .required()
         .messages({
             "string.empty": "First name is required",
-            "string.min": "First name must be at least 2 characters",
+            "string.min": "First name must be at least 1 characters",
             "string.max": "First name must be at most 30 characters",
             "string.pattern.base": "First name must contain only letters",
             "any.required": "First name is required"
